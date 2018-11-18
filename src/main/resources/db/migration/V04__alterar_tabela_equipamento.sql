515ae90148552615a6205021fccb503aecbc6bbd
dattebayoRob <robson.william65@gmail.com>
2018-11-18 19:02:32 -0300

    uatafoque fix

--- src/main/resources/db/migration/V04__alterar_tabela_equipamento.sql
+++ src/main/resources/db/migration/V04__alterar_tabela_equipamento.sql
@@ -4,6 +4,6 @@ CREATE TABLE equipamento_status(
 );
 INSERT INTO equipamento_status(nome) VALUES('RECEBIDO'),('EM MANUTENÇÃO'),('EM HOMOLOGAÇÃO'),('AGUARDANDO ENTREGA'),('ENTREGUE');
 ALTER TABLE equipamento ADD COLUMN id_status BIGINT;
-pdate equipamento SET  id_status = '1' WHERE equipamento.id > 0;
+UPDATE equipamento SET  id_status = '1' WHERE equipamento.id > 0;
 ALTER TABLE equipamento ADD CONSTRAINT equipamento_id_status_fkey FOREIGN KEY (id_status)
 	REFERENCES equipamento_status(id), ALTER COLUMN id_status SET NOT NULL;
