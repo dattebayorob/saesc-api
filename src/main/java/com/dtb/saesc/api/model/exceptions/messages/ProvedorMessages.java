60a1f15cab49584c462e69a649e0a938fa0afada
dattebayoRob <robson.william65@gmail.com>
2019-03-17 00:02:30 -0300

    Error constants of provedor

---
+++ src/main/java/com/dtb/saesc/api/model/exceptions/messages/ProvedorMessages.java
@@ -0,0 +1,6 @@
+package com.dtb.saesc.api.model.exceptions.messages;
+
+public class ProvedorMessages {
+	public static final String CNPJ_JA_CADASTRADO = "CNPJ já cadastrado.";
+	public static final String PROVEDOR_NAO_ENCONTRADO = "Nenhum provedor encontrado para o id informado";
+}
