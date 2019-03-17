5cbf0fafd72adc6eca757bb805c4a482f89f5cfd
dattebayoRob <robson.william65@gmail.com>
2019-03-17 19:17:38 -0300

    Update Equipamento's service and endpoint

--- src/main/java/com/dtb/saesc/api/services/EquipamentoService.java
+++ src/main/java/com/dtb/saesc/api/services/EquipamentoService.java
@@ -9,12 +9,15 @@ import com.dtb.saesc.api.model.entities.Equipamento;
 import com.dtb.saesc.api.model.entities.Funcionario;
 import com.dtb.saesc.api.model.repositories.custom.filter.EquipamentoFilter;
 
+import io.vavr.control.Either;
+
 public interface EquipamentoService {
+	
 	public Optional<Equipamento> buscarPeloId(Long id);
 
-	public Page<Equipamento> buscarPaginaPorFiltros(EquipamentoFilter filter, Pageable pageable);
+	public Optional<Page<Equipamento>> buscarPaginaPorFiltros(EquipamentoFilter filter, Pageable pageable);
 
-	public Equipamento persistir(Equipamento equipamento, String comentario, Funcionario funcionario);
+	public Either<RuntimeException, Equipamento> persistir(Equipamento equipamento, String comentario, Funcionario funcionario);
 
 	public Boolean existePeloId(Long id);
 }
