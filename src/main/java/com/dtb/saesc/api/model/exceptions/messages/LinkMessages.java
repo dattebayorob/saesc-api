c384f4435ac566569e34f6ef1b6a1757b55e3c93
dattebayoRob <robson.william65@gmail.com>
2019-03-17 10:17:45 -0300

    Link's Errors constants

---
+++ src/main/java/com/dtb/saesc/api/model/exceptions/messages/LinkMessages.java
@@ -0,0 +1,5 @@
+package com.dtb.saesc.api.model.exceptions.messages;
+
+public class LinkMessages {
+	public final static String LINK_NAO_ENCONTRADO = "Não foi encontrado nenhum link para o ip informado.";
+}
