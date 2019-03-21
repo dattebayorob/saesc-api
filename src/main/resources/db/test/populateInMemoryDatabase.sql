0b1f39c4aca44a1f659df64a20efeeeee52b2071
dattebayoRob <robson.william65@gmail.com>
2019-03-21 11:04:53 -0300

    Update sql script with new table scheme

--- src/main/resources/db/test/populateInMemoryDatabase.sql
+++ src/main/resources/db/test/populateInMemoryDatabase.sql
@@ -1,9 +1,9 @@
-INSERT INTO link_provedor(nome, cnpj, data_criacao, data_atualizacao) VALUES('Provedor Fake','00000000000000',CURRENT_DATE,CURRENT_DATE);
-INSERT INTO escola(nome,crede,inep,data_criacao,data_atualizacao)
+INSERT INTO provedor(nome, cnpj, data_criacao, data_atualizacao) VALUES('Provedor Fake','00000000000000',CURRENT_DATE,CURRENT_DATE);
+INSERT INTO instituicao(nome,crede,inep,data_criacao,data_atualizacao)
  VALUES('FAKE','SEDUC','00000000',CURRENT_DATE,CURRENT_DATE);
-INSERT INTO escola_link(ip,id_provedor,id_escola)
+INSERT INTO instituicao_link(ip,id_provedor,id_instituicao)
  VALUES('200.217.200.xxx','1','1');
-INSERT INTO funcionario(nome,cpf,email,senha,perfil,id_escola,data_criacao,data_atualizacao) 
+INSERT INTO funcionario(nome,cpf,email,senha,perfil,id_instituicao,data_criacao,data_atualizacao) 
  VALUES('Fake','00000000000','fake@fake',
  	'$2a$10$iqpnKvFQvKlBWghd5iw6X.v50j9QY7AgCceSEKB0hbvmA83IAofCS','ROLE_TECNICO','1',CURRENT_DATE,CURRENT_DATE);
  INSERT INTO equipamento_marca(nome)VALUES('DELL');
@@ -11,7 +11,7 @@ INSERT INTO funcionario(nome,cpf,email,senha,perfil,id_escola,data_criacao,data_
 	VALUES('OPTIPLEX 780','1','COMPUTADOR'),('OPTIPLEX 7010','1','COMPUTADOR');
  INSERT INTO equipamento_status(nome) VALUES('RECEBIDO'),('EM MANUTENÇÃO'),('EM HOMOLOGAÇÃO'),('AGUARDANDO ENTREGA'),('ENTREGUE');
 	VALUES('OPTIPLEX 780','1','COMPUTADOR');
-INSERT INTO equipamento(id_modelo,id_escola,id_status,descricao,data_criacao)
+INSERT INTO equipamento(id_modelo,id_instituicao,id_status,descricao,data_criacao)
 	VALUES('1','1','1','Equipamento de Testes 1',CURRENT_DATE);
 INSERT INTO equipamento_historico(id_equipamento,id_funcionario,data)
 	VALUES('1','1',CURRENT_DATE);
