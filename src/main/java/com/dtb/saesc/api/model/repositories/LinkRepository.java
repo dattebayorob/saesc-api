e9229759b00ad3bcf2d2bcdc52f86aa283159c9a
dattebayoRob <robson.william65@gmail.com>
2019-03-23 21:26:11 -0300

    Add findByIpAndProvedor to check if already exist a Provedor link with passed Ip

--- src/main/java/com/dtb/saesc/api/model/repositories/LinkRepository.java
+++ src/main/java/com/dtb/saesc/api/model/repositories/LinkRepository.java
@@ -1,8 +1,12 @@
 package com.dtb.saesc.api.model.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dtb.saesc.api.model.entities.Link;
+import com.dtb.saesc.api.model.entities.Provedor;
 
 public interface LinkRepository extends JpaRepository<Link, Long> {
+	Optional<Link> findByIpAndProvedor(String ip,Provedor provedor);
 }
--- src/test/java/com/dtb/saesc/api/model/repositories/LinkRepositoryTest.java
+++ src/test/java/com/dtb/saesc/api/model/repositories/LinkRepositoryTest.java
@@ -1,7 +1,11 @@
 package com.dtb.saesc.api.model.repositories;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Optional;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -24,38 +28,51 @@ import com.dtb.saesc.api.model.entities.Provedor;
 public class LinkRepositoryTest {
 	@Autowired
 	private LinkRepository repository;
-	
+
 	private Link link;
 	private static final Log log = LogFactory.getLog(LinkRepositoryTest.class);
 	private static final Long ID_ESCOLA = Long.valueOf(1);
-	
+
 	@Before
 	public void init() {
 		log.info("Instanciação e Persistencia inicial pros testes");
-		
-		link = Link
-				.builder()
-					.instituicao(Instituicao.builder().id(ID_ESCOLA).build())
-					.ip("172.xxx.xxx.xx1")
-					.provedor(Provedor.builder().id(Long.valueOf(1)).build())
-				.build();
+
+		link = Link.builder().instituicao(Instituicao.builder().id(ID_ESCOLA).build()).ip("172.xxx.xxx.xx1")
+				.provedor(Provedor.builder().id(Long.valueOf(1)).build()).build();
 		repository.save(link);
 	}
+
 	@After()
 	public void finish() {
 		log.info("Entidade removida no test, não há o que deletar");
 	}
+
 	@Test
 	public void testSave() {
 		assertNotNull(link.getId());
 	}
+
 	@Test
 	public void testFindById() {
 		assertNotNull(repository.findById(link.getId()).get());
 	}
+
 	@Test
-	public void deleteById() {
+	public void testDeleteById() {
 		repository.deleteById(link.getId());
 		assertFalse(repository.findById(link.getId()).isPresent());
+	}
+
+	@Test
+	public void testFindByIdAndProvedor() {
+		Optional<Link> link = repository.findByIpAndProvedor("200.217.200.xxx",
+				Provedor.builder().id(this.link.getProvedor().getId()).build());
+		assertTrue(link.isPresent());
+	}
+	@Test
+	public void testFindByIdAndProvedor_WithNonCadastratedIp() {
+		Optional<Link> link = repository.findByIpAndProvedor("ip",
+				Provedor.builder().id(this.link.getProvedor().getId()).build());
+		assertFalse(link.isPresent());
 	}
 }
