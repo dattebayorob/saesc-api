3842e934d6fd8d6f84b7b479373561ff1002751d
dattebayoRob <robson.william65@gmail.com>
2019-03-23 22:00:33 -0300

    Post and Put on Link endpoint

--- src/main/java/com/dtb/saesc/api/controller/LinkController.java
+++ src/main/java/com/dtb/saesc/api/controller/LinkController.java
@@ -1,12 +1,24 @@
 package com.dtb.saesc.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dtb.saesc.api.model.converters.Converter;
 import com.dtb.saesc.api.model.dtos.LinkDto;
 import com.dtb.saesc.api.model.entities.Link;
+import com.dtb.saesc.api.model.exceptions.ResourceNotFoundException;
+import com.dtb.saesc.api.model.exceptions.messages.LinkMessages;
+import com.dtb.saesc.api.model.response.Response;
+import com.dtb.saesc.api.model.response.impl.ResponseData;
+import com.dtb.saesc.api.model.response.impl.ResponseError;
 import com.dtb.saesc.api.services.LinkService;
 
 @RestController
@@ -18,6 +30,32 @@ public class LinkController {
 	
 	@Autowired
 	private Converter<Link, LinkDto> converter;
+	
+	@PostMapping
+	public ResponseEntity<Response> adicionar(@Validated @RequestBody LinkDto dto){
+		return new ResponseEntity<Response>(
+				ResponseData.data(service
+						.adicionar(converter
+								.toEntity(Link.class)
+								.convert(dto))
+						.fold(ResponseError::exception,
+								l -> converter.toDto(LinkDto.class).convert(l))
+				), HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Response> atualizar(@PathVariable("id") Long id, @Validated @RequestBody LinkDto dto){
+		Link link = service.buscarPeloId(id)
+				.orElseThrow(() -> new ResourceNotFoundException(LinkMessages.LINK_NAO_ENCONTRADO));
+		dto.setId(link.getId());
+		String ip = link.getIp();
+		return ResponseEntity.ok(
+				ResponseData.data(service.atualizar(
+						converter.toEntity(Link.class).convert(dto),
+						ip).fold(ResponseError::exception,
+								l -> converter.toDto(LinkDto.class).convert(l))
+				));
+	}
 
 	
 }
